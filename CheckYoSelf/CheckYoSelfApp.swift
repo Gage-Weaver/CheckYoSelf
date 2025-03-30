e28cdc5c16b8298692b17131e6bc5e5f906a9d58
Carlos Mbendera <carlosmbendera@gmail.com>
2025-03-29 19:33:48 -0500

    Initial Commit

---
+++ CheckYoSelf/CheckYoSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckYoSelfApp.swift
+//  CheckYoSelf
+//
+//  Created by Carlos Mbendera on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckYoSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
